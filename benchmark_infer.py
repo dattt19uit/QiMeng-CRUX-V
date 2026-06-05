c35022fb725e9c493f7d85585cfd30c889c109bf
dat_ttan <dattt.19@grad.uit.edu.vn>
2026-06-05 11:37:47 +0700

    Update benchmark_infer.py

--- benchmark_infer.py
+++ benchmark_infer.py
@@ -188,7 +188,7 @@ class VerilogGenBenchmark:
         self.use_template = use_template
         self.model_path = model_path
         # self.llm = LLM(model=model_path) #, tensor_parallel_size=8)
-        self.llm = LLM(model=model_path, gpu_memory_utilization=0.85)
+        self.llm = LLM(model=model_path, tensor_parallel_size=2, gpu_memory_utilization=0.85)
 
 
     
