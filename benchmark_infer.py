7f060f9cea331c3aa8d92968bd94594561a95b28
dat_ttan <dattt.19@grad.uit.edu.vn>
2026-06-05 14:04:19 +0700

    update attention_backend="TRITON_ATTN"

--- benchmark_infer.py
+++ benchmark_infer.py
@@ -193,7 +193,7 @@ class VerilogGenBenchmark:
             tensor_parallel_size=1,           # ← Giảm xuống 1 GPU trước (rất quan trọng)
             gpu_memory_utilization=0.88,      # tăng nhẹ nhưng an toàn
             enforce_eager=True,               # ← BẮT BUỘC
-            attention_backend="xformers",     # ← BẮT BUỘC
+            attention_backend="TRITON_ATTN",     # ← BẮT BUỘC
             max_model_len=16384,              # giảm tạm để tiết kiệm memory
             dtype="float16",
             trust_remote_code=True,
